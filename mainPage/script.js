7c16c71b8a982d718dd40d80a07f2938e505b141
KaKaz0815 <kkaz0815@byu.edu>
2020-01-10 23:53:10 -0700

    1-10-2020 11:53

---
+++ mainPage/script.js
@@ -0,0 +1,46 @@
+let daynames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"];
+let months =[
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"];
+
+let d = new Date();
+let dayName = daynames [d.getDay()];
+let monthName = months [d.getMonth()];
+let year = d.getFullYear();
+let fulldate = dayName + "," + monthName + " " + d.getDate() + "," + year;
+
+
+document.getElementById("currentdate").textcontent = fulldate;
+
+try{
+    let options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+        year: "numeric"
+    };
+    document.getElementById(
+        "currentdate2"
+    ).textContent = new Date().toLocaleDateString("en-US", options);
+    }
+    catch(e){
+        alert("Error with code or browser does no support Locale");
+    }
+}
