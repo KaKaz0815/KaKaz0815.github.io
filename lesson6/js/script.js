8d3795d4c4b5657b661f28a880b8205997b4da80
KaKaz0815 <kkaz0815@byu.edu>
2020-02-18 19:16:01 -0700

    2.18.2020

--- lesson6/js/script.js
+++ lesson6/js/script.js
@@ -34,22 +34,8 @@ console.log(fulldate);
 
 
 
-//Humberger Menu Button
-
-const hambutton = document.querySelector(".ham");
-hambutton.addEventListener("click", toggleMenu, false);
-
-function toggleMenu() {
-  document.querySelector(".navigation").classList.toggle("responsive");
-}
 
 
 
 
-//wind Chill calculator
 
-
-const temperature = parseFloat(document.getElementById("temp").textContent);
-const windSpeed = parseFloat(document.getElementById("WSpeed").textContent);
- 
-document.getElementById("windChill").textContent =  35.74 + (0.6215 * temperature) + (0.4275 * temperature - 35.75)  *  windSpeed ^ 0.16;
