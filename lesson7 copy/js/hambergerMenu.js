0298441bcc6949873a22c72eb1edd6298a084db8
KaKaz0815 <kkaz0815@byu.edu>
2020-02-25 20:15:51 -0700

    2-25-2020

---
+++ lesson7 copy/js/hambergerMenu.js
@@ -0,0 +1,8 @@
+//Hamberger Menu Button
+
+const hambutton = document.querySelector(".ham");
+hambutton.addEventListener("click", toggleMenu, false);
+
+function toggleMenu() {
+  document.querySelector(".navigation").classList.toggle("responsive");
+}
